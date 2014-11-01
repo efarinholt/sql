793e32ffd45ab63e6c7307207e18b945addda81e
efarinholt <evan@fireinspace.net>
2014-11-01 17:37:42 -0400

    Create sql

---
+++ sqlQueries.sql
@@ -0,0 +1,15 @@
+-- Create New Database
+CREATE DATABASE db_DatabaseName; 		
+
+-- Select DB to use
+USE db_DatabaseName;
+
+-- Create New Table
+CREATE TABLE tbl_TableName (
+	user_id int NOT NULL PRIMARY KEY AUTO_INCREMENT, -- auto-increment id/key
+	user_first varchar(255) DEFAULT NULL, 
+	user_last varchar(255) DEFAULT NULL,
+	user_email varchar(255) DEFAULT NULL,
+	user_timestamp datetime DEFAULT NULL
+); 			
+
