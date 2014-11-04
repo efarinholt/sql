dce9fd25398b0b9466af107ca07fae13865bec19
Evan Farinholt <evan@fireinspace.net>
2014-11-04 14:46:20 -0500

    Update sqlQueries.sql

--- sqlQueries.sql
+++ sqlQueries.sql
@@ -7,8 +7,8 @@ USE db_DatabaseName;
 -- Create New Table
 CREATE TABLE tbl_TableName (
 	user_id int NOT NULL PRIMARY KEY AUTO_INCREMENT, -- auto-increment id/key
-	user_first varchar(255) DEFAULT NULL, 
-	user_last varchar(255) DEFAULT NULL,
+	user_first varchar(50) DEFAULT NULL, 
+	user_last varchar(50) DEFAULT NULL,
 	user_email varchar(255) DEFAULT NULL,
 	user_timestamp datetime DEFAULT NULL
 ); 			
